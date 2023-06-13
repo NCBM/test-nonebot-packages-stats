4a06da9eeaec59061875e30dedf0b5bd13491bb1
worldmozara <worldmozara@163.com>
2023-06-13 22:11:03 +0800

    Update

--- plugin_stats.py
+++ plugin_stats.py
@@ -2,8 +2,25 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 from google.cloud import bigquery
 import requests
 
+NoneBotMeta = dict[
+    {
+        "module_name": str,
+        "project_link": str,
+        "name": str,
+        "desc": str,
+        "author": str,
+        "homepage": str,
+        "tags": list[dict[{"label": str, "color": str}]],
+        "is_official": bool
+    }
+]
+
 # Construct a BigQuery client object.
 client = bigquery.Client()
+
+print("*** Downloading plugins list...")
+data: list[NoneBotMeta] = requests.get("https://v2.nonebot.dev/plugins.json").json()
+print("=== Downloaded plugins list")
 
 
 def get_downloads_dry(pkg: str, interval: int = 30) -> int:
@@ -27,9 +44,6 @@ def get_downloads_dry(pkg: str, interval: int = 30) -> int:
 
 
 def get_plugin_list() -> list[str]:
-    print("*** Downloading plugins list...")
-    data = requests.get("https://v2.nonebot.dev/plugins.json").json()
-    print("=== Downloaded plugins list")
     return [x["project_link"] for x in data]
 
 
@@ -40,10 +54,11 @@ def get_and_store(pkg: str):
     downloads.append((pkg, get_downloads_dry(pkg)))
 
 
-with ThreadPoolExecutor(max_workers=5) as exc:
+with ThreadPoolExecutor(max_workers=8) as exc:
     tasks = [exc.submit(get_and_store, pkg) for pkg in get_plugin_list()]
+    co = 0
     for _ in as_completed(tasks):
-        ...
+        print(f"=== {co := co + 1}/{len(data)} done")
 
 
 downloads.sort(key=lambda x: x[1], reverse=True)
