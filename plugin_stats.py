c2aab1338073d20d0301d14787958da17e0d11a8
worldmozara <worldmozara@163.com>
2023-06-13 19:56:43 +0800

    Update

--- plugin_stats.py
+++ plugin_stats.py
@@ -1,4 +1,6 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from google.cloud import bigquery
+import requests
 
 # Construct a BigQuery client object.
 client = bigquery.Client()
@@ -24,6 +26,26 @@ def get_downloads_dry(pkg: str, interval: int = 30) -> int:
     raise RuntimeError(f"failed to get data of {pkg!r}")
 
 
-for pkg in ("nonebot2",):
-    down = get_downloads_dry(pkg)
+def get_plugin_list() -> list[str]:
+    print("*** Downloading plugins list...")
+    data = requests.get("https://v2.nonebot.dev/plugins.json").json()
+    print("=== Downloaded plugins list")
+    return [x["project_link"] for x in data]
+
+
+downloads: list[tuple[str, int]] = []
+
+
+def get_and_store(pkg: str):
+    downloads.append((pkg, get_downloads_dry(pkg)))
+
+
+with ThreadPoolExecutor(max_workers=5) as exc:
+    tasks = [exc.submit(get_and_store, pkg) for pkg in get_plugin_list()]
+    for _ in as_completed(tasks):
+        ...
+
+
+downloads.sort(key=lambda x: x[1], reverse=True)
+for pkg, down in downloads:
     print(f"{pkg}:", down)
